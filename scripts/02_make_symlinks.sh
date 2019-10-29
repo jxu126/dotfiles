bd3b4bbfc9aa4f615928e34a86b5cba18b029d70
Ian Phipps <phippsian@gmail.com>
2019-10-29 19:06:21 +0000

    add tmux conf

--- scripts/02_make_symlinks.sh
+++ scripts/02_make_symlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles/config_files                    # dotfiles directory
 olddir=~/dotfiles_old/config_files             # old dotfiles backup directory
-files="vimrc zshrc mac_zshrc linux_zshrc inputrc gitignore_global ignore"    # list of files/folders to symlink in homedir
+files="vimrc zshrc mac_zshrc linux_zshrc inputrc gitignore_global ignore tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
